2356678eb9a57c72559083a914ac041a1addda07
LokosPL <58862558+LokosPL@users.noreply.github.com>
2026-09-20 01:56:03 +0200

    Remove obsolete Google web client config (#26)
    Remove the unused website Google client id and refresh the PWA shell after code-only pairing became the only employee web authentication path.

--- assets/google-config.js
+++ assets/google-config.js
@@ -1,4 +1,3 @@
 window.LOCKON_WEB_AUTH = Object.freeze({
-  googleClientId: '996585439932-uaoifil2rud5q6h0ibp1coqn8vi0p3u3.apps.googleusercontent.com',
   apiBaseUrl: 'https://br-steep-bonus-b1f1qh8u-lockonapi.compute.c-5.eu-central-1.aws.neon.tech'
 });
